2113649e1e8c6ba1d44300d260cf10f8c9c1a0e8
Vash <jpodesta.tkd@gmail.com>
2023-10-10 19:11:16 -0300

    modulo 8 terminado

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,16 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   let a = nombre.split("")
+   a[0] = a[0].toUpperCase()
+   return a.join("")
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +22,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb (num1, num2)
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,13 +31,21 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   let b = 0
+   for (let i = 0; i <= arrayOfNumbers.length; i++){
+      b += i
+   }
+   cb(b)
 }
-
 function forEach(array, cb) {
    // Recibes un arreglo y un callback.
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach((num)=>{
+      cb(num)
+   })
+   return array
 }
 
 function map(array, cb) {
@@ -40,12 +53,22 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   let asd = array.map((as)=>{
+      return  cb(as)
+   })
+   return asd
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let letra = arrayOfStrings.filter(function(asdf){
+      if (asdf.startsWith("a")){
+         return asdf
+      }
+   })
+   return letra
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
